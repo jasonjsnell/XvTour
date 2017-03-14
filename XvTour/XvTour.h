83b4192162bcafe21090ec88ef77aa577960fc8e
jasonjsnell <snell.jason@gmail>
2017-03-14 06:17:09 -0700

    Initial Commit

---
+++ XvTour/XvTour.h
@@ -0,0 +1,19 @@
+//
+//  XvTour.h
+//  XvTour
+//
+//  Created by Jason Snell on 3/14/17.
+//  Copyright © 2017 Jason J. Snell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XvTour.
+FOUNDATION_EXPORT double XvTourVersionNumber;
+
+//! Project version string for XvTour.
+FOUNDATION_EXPORT const unsigned char XvTourVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XvTour/PublicHeader.h>
+
+
